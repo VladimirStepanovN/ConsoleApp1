92c6cffe79304976cb8b67bfe4ff105a974458d1
VladimirStepanovN <Vladimir.Stepanov.Dev@yandex.ru>
2023-05-15 00:34:53 +0500

    Ответ задание 3.5.3

--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\tПривет,\n мир!");
+            int age = 5;
+            Console.WriteLine($"Мой возраст {age}");
         }
 	}
 }
