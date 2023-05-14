7944a4532379d6b25d1964305a67d11830925da6
VladimirStepanovN <Vladimir.Stepanov.Dev@yandex.ru>
2023-05-15 00:49:14 +0500

    Ответ задание 3.6.5

--- Program.cs
+++ Program.cs
@@ -4,8 +4,10 @@
     {
         static void Main(string[] args)
         {
-            double result = 5.0 / 2.0 * 3.0;
-            Console.WriteLine(result);
+            int b = 10;
+            int k = -5;
+            b -= k;
+            Console.WriteLine(b);
         }
 	}
 }
