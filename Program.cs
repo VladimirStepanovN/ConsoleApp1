00948c33a27ed09d5ec12d4d657d73d88d0f89a4
VladimirStepanovN <Vladimir.Stepanov.Dev@yandex.ru>
2023-05-15 01:08:05 +0500

    Ответ задание 3.7.4

--- Program.cs
+++ Program.cs
@@ -4,8 +4,14 @@
     {
         static void Main(string[] args)
         {
-            byte age = 255;
-            Console.WriteLine(++age);
+            Console.Write("Enter yuor name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            byte age = checked((byte) int.Parse(Console.ReadLine()));
+            Console.WriteLine("Your name is {0} and age is {1} ", name, age);
+            Console.Write("What is your favorite day of week? ");
+            DayOfWeek day = (DayOfWeek) int.Parse(Console.ReadLine());
+            Console.WriteLine("Your favorite day is {0}", day);
         }
 	}
 }
