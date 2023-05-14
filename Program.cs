5b5d5e8e25b6f06dc92f6e2cb38a0ca9288fb7c5
VladimirStepanovN <Vladimir.Stepanov.Dev@yandex.ru>
2023-05-15 00:47:07 +0500

    Ответ задание 3.6.3

--- Program.cs
+++ Program.cs
@@ -4,8 +4,8 @@
     {
         static void Main(string[] args)
         {
-            double result = 5.0 / 2.0;
-            Console.WriteLine("5 / 2 = {0}", result);
+            double result = 5.0 / 2.0 * 3.0;
+            Console.WriteLine(result);
         }
 	}
 }
