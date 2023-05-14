a6a2398deb8da9d053ef27d5f73d936a00b565d3
VladimirStepanovN <Vladimir.Stepanov.Dev@yandex.ru>
2023-05-15 00:19:34 +0500

    Ответ задание 3.4.2

--- Program.cs
+++ Program.cs
@@ -5,16 +5,18 @@
         static void Main(string[] args)
         {
 
-			string MyName = "Jane";
-			byte MyAge = 27;
-			bool HaveIApet = true;
-			double MyShoeSize = 37.5;
+			DaysOfWeek MyFavoriteDay;
 
-			Console.WriteLine("My name is " + MyName);
-			Console.WriteLine("MyAge " + MyAge);
-			Console.WriteLine("Do I have a pet? " + HaveIApet);
-			Console.WriteLine("My shoe size is " + MyShoeSize);
+			MyFavoriteDay = DaysOfWeek.Friday;
 
+			Console.WriteLine(MyFavoriteDay);
 		}
+	}
+	enum DaysOfWeek : byte
+	{
+		Tuesday,
+		Monday,
+		Wednesday,
+		Friday
 	}
 }
